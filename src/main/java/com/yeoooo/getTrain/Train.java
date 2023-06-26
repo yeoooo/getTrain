7d4db19427718bc1fa4a9965a74ff1dccac0ed61
yeoooo <ruisya330@gmail.com>
2023-06-26 21:25:20 +0900

    feat : 열차 정보 크롤링 추가
    List<Train> 반환
    Train 필드 추가
    TrainType 추가

--- src/main/java/com/yeoooo/getTrain/Train.java
+++ src/main/java/com/yeoooo/getTrain/Train.java
@@ -1,14 +1,25 @@
 package com.yeoooo.getTrain;
 
+import com.yeoooo.getTrain.crawling.TrainType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
+@ToString
+@Builder
 public class Train {
+    private int train_id;
+    private String division;
+    private TrainType trainType;
     private String from;
+    private String depart_time;
     private String to;
-    private LocalDateTime arrival;
+    private String arrival_time;
+    private String cost;
+    private String time_cost;
 }
--- src/main/java/com/yeoooo/getTrain/crawling/CrawlUtil.java
+++ src/main/java/com/yeoooo/getTrain/crawling/CrawlUtil.java
@@ -2,18 +2,27 @@ package com.yeoooo.getTrain.crawling;
 
 import com.yeoooo.getTrain.Train;
 import com.yeoooo.getTrain.exception.LoginFailedException;
+import lombok.Getter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Timer;
 
+@Getter
 public class CrawlUtil {
+
     private ChromeOptions options;
     private WebDriver driver;
 
@@ -30,41 +39,90 @@ public class CrawlUtil {
      * @param time
      * @return ArrayList<Train> 기차 표 조회
      */
-    public ArrayList<Train> get_arrivals(String from, String to, String time){
+    public ArrayList<Train> get_arrivals(String from, String to, LocalDateTime time, int amount) throws InterruptedException {
 
         ArrayList<Train> trains = new ArrayList<>();
 
 		driver.get("https://www.letskorail.com/ebizprd/EbizPrdTicketpr21100W_pr21110.do");
+
         WebElement input_start = driver.findElement(By.id("start"));
+        input_start.clear();
+
         WebElement input_get = driver.findElement(By.id("get"));
+        input_get.clear();
+
         WebElement input_sYear = driver.findElement(By.id("s_year"));
+
         WebElement input_sMonth = driver.findElement(By.id("s_month"));
+
         WebElement input_sDay = driver.findElement(By.id("s_day"));
+
         WebElement input_sHour = driver.findElement(By.id("s_hour"));
+
 //        WebElement input_sweek = driver.findElement(By.id("s_week"));
         WebElement btnInq = driver.findElement(By.className("btn_inq"));
-        String source = driver.getPageSource();
+//        WebElement peop01 = driver.findElement(By.id("peop01")); // 어른 인원 수
+        WebElement aElement = driver.findElement(By.className("btn_inq")).findElement(By.tagName("a"));
 
-//        input_start.sendKeys(from);
-//        input_get.sendKeys(to);
+        input_start.sendKeys(from);
+        input_get.sendKeys(to);
 
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss.S");
-        LocalDateTime target_date = LocalDateTime.parse(time, dateTimeFormatter);
-        System.out.println();
+        input_sYear.sendKeys(String.valueOf(time.getYear()));
+        input_sMonth.sendKeys(String.valueOf(time.getMonth()));
+        input_sDay.sendKeys(String.valueOf(time.getDayOfMonth()));
+        input_sHour.sendKeys(String.valueOf(time.getHour()));
 
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript(aElement.getAttribute("href"));
 
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("tableResult")));
 
-//        System.out.println("source = " + source);
-//        System.out.println("input_start = " + input_start.getText());
-//        System.out.println("input_get = " + input_get.getText());
-//        System.out.println("input_sYear = " + input_sYear.getText());
-//        System.out.println("input_sMonth = " + input_sMonth.getText());
-////        System.out.println("input_sweek = " + input_sweek.getText());
-//        System.out.println("btnInq = " + btnInq.getText());
+        WebElement tableResult = driver.findElement(By.id("tableResult"));
+        List<WebElement> tableResult_tbody = tableResult.findElements(By.tagName("tr"));
 
-        //파싱 및 Train 객체 생성
+        for (WebElement td :
+                tableResult_tbody) {
+            List<WebElement> table_data = td.findElements(By.tagName("td"));
 
-        driver.quit();
+            if (table_data.size() == 0) {
+                continue;
+            }
+
+            String division = table_data.get(0).getText();
+            String[] train_info = table_data.get(1).getText().split("\n");
+            String[] departure_info = table_data.get(2).getText().split("\n");
+            String[] arrival_info = table_data.get(3).getText().split("\n");
+            String[] cost = table_data.get(8).getText().split("\n");
+            String[] time_cost = table_data.get(13).getText().split("\n");
+
+            TrainType type = null;
+            int train_id = 0;
+
+            if(train_info.length == 2){
+                type = TrainType.KTX;
+                train_id = Integer.parseInt(train_info[1]);
+            }else{
+                if (train_info[0].startsWith("10")) {
+                    type = TrainType.ITX;
+                }else{
+                    type = TrainType.MUGUNGHWA;
+                }
+                train_id = Integer.parseInt(train_info[0]);
+            }
+            trains.add(
+                Train.builder()
+                        .division(division)
+                        .trainType(type)
+                        .train_id(train_id)
+                        .from(departure_info[0])
+                        .to(arrival_info[0])
+                        .depart_time(departure_info[1])
+                        .arrival_time(arrival_info[1])
+                        .cost(cost[0])
+                        .time_cost(time_cost[0])
+                        .build());
+        }
         return trains;
     }
     public boolean login(LoginType loginType, String id, String pw) {
@@ -116,7 +174,6 @@ public class CrawlUtil {
         if (driver.getTitle().equals("로그인")) {
             throw new LoginFailedException();
         }
-        driver.quit();
         return true;
     }
 
