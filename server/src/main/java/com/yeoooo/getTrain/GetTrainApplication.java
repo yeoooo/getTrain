4f41e5dda749174ce0156444f1dfef808d4aa727
yeoooo <ruisya330@gmail.com>
2023-07-07 22:01:04 +0900

    fix: 메인 메서드 내용 복구

--- server/src/main/java/com/yeoooo/getTrain/GetTrainApplication.java
+++ server/src/main/java/com/yeoooo/getTrain/GetTrainApplication.java
@@ -1,18 +1,11 @@
 package com.yeoooo.getTrain;
 
-import com.yeoooo.getTrain.util.RequestTimer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 public class GetTrainApplication {
 	public static void main(String[] args){
-//		SpringApplication.run(GetTrainApplication.class, args);
-		RequestTimer requestTimer = new RequestTimer(3000);
-		requestTimer.startTimer();
-
+		SpringApplication.run(GetTrainApplication.class, args);
 	}
 }
