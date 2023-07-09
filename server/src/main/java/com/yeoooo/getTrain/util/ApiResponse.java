0c8bf60ee579c55ec40675567c7fc4b95775da81
yeoooo <ruisya330@gmail.com>
2023-07-09 22:39:29 +0900

    feat: ApiResponse 변경
    int -> HttpStatus
    객체로 변경

--- server/src/main/java/com/yeoooo/getTrain/util/ApiResponse.java
+++ server/src/main/java/com/yeoooo/getTrain/util/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.yeoooo.getTrain.util;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,24 +10,24 @@ import java.time.LocalDateTime;
 public class ApiResponse<T> {
 
     private T data ;
-    private int code;
+    private HttpStatus code;
     private LocalDateTime timeStamp;
-    public ApiResponse(int code, T data) {
+    public ApiResponse(HttpStatus code, T data) {
         this.code = code;
         this.data = data;
         this.timeStamp = LocalDateTime.now();
     }
 
-    public ApiResponse(int code) {
+    public ApiResponse(HttpStatus code) {
         this.code = code;
         this.timeStamp = LocalDateTime.now();
     }
 
     public static <T> ApiResponse<T> ok(T data) {
-        return new ApiResponse<>(200, data);
+        return new ApiResponse<>(HttpStatus.OK, data);
     }
 
-    public static <T> ApiResponse fail(int code, T errData) {
+    public static <T> ApiResponse fail(HttpStatus code, T errData) {
         return new ApiResponse<>(code, errData);
     }
 
