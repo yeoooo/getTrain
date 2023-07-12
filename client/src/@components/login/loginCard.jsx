3e1eeca9c2431a1c4bb5510ad8300153d24f1fe7
ererink <yelkim0210@gmail.com>
2023-07-12 23:30:21 +0900

    라우터 설정

--- client/src/@components/login/loginCard.jsx
+++ client/src/@components/login/loginCard.jsx
@@ -17,18 +17,18 @@ function LoginCard(props){
 
             <form action='#' method='get' className='login-input'>
                 <div>
-                    <img src='../../src/assets/Phone-icon.png' alt='Phone icon' width={20} height={20}/>
+                    <img src='../../src/assets/Phone-icon.png' alt='Phone icon'/>
                     <input type='number' name='phone' placeholder='번호를 입력해주세요'/>
                 </div>
                 
                 <div>
-                    <img src='../../src/assets/Lock-icon.png' alt='Lock icon' width={20} height={20}/>
+                    <img src='../../src/assets/Lock-icon.png' alt='Lock icon'/>
                     <input type='password' name='password' placeholder='비밀번호를 입력해주세요'/>
                 </div>
                 
                 <button type='submit'>LOGIN</button>
+                <a href='../../src/@pages/SearchFormPage'> 열차 조회 페이지 </a>
             </form>
-
         </div> 
     );
 }
