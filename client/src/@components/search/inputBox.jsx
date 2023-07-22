99eefd55ae28db6b6681949d68e9cdeb259a7ac1
ererink <yelkim0210@gmail.com>
2023-07-22 23:07:57 +0900

    아코디언 1차 구현

--- client/src/@components/search/inputBox.jsx
+++ client/src/@components/search/inputBox.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../style/searchForm.css'
 import styled from 'styled-components';
 import InputSection from './inputSection'
 
+
 function InputBox(props){
-    return(
-        <div className='input-warpper'>
-            <div className='input-box'>
-                <div className='input-box-header'>
-                    <img src={props.icon} className='train-icon' alt='icon'/>
-                    <h3>{props.title}</h3>
-                    <a href='#'><img src='../../src/assets/arrow.png' className='arrow-icon' alt='arrow icon' /></a>
+    // 아코디언 감지 (기본은 열려있는 상태)
+    const [isOpen, setIsOpen] = useState(true);
+    console.log(isOpen);
+    // 체크박스에서 선택한 항목을 자식 컴포넌트에서 가져오기
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    // 아코디언 토글
+    const handleSelectedItem = (item) => {
+        setSelectedItem(item);
+        console.log("setIsOpen 상태 변함")
+        handleAccordionToggle();
+    }
+    const handleAccordionToggle = () => {
+        setIsOpen((preOpen) => !preOpen);
+    };
+    // 기본 상태 (아코디언이 열려있는 상태)
+    if(isOpen){
+        return(
+            <div className='input-warpper'>
+                <div className='input-box'>
+                    <div className='input-box-header'>
+                        <img src={props.icon} className='train-icon' alt='icon'/>
+                        <h3>{props.title}</h3>
+                        <a href='#'><img src='../../src/assets/arrow.png' className='arrow-icon' alt='arrow icon'/></a>
+                    </div>
+                    <InputSection title={props.title} handleSelectedItem={handleSelectedItem} isOpen={isOpen}/>
                 </div>
-                <InputSection title={props.title}/>
+                <p>{selectedItem}</p>
             </div>
-        </div>
-    );
+        );
+    } 
+    // 입력값을 넣은 후 아코디언이 접혀져 있는 상태
+    else {
+        return(
+            <div className='input-warpper'>
+                <div className='input-box'>
+                    <div className='input-box-header'>
+                        <img src={props.icon} className='train-icon' alt='icon'/>
+                        <h3>{selectedItem}</h3>
+                        <a href='#'><img src='../../src/assets/arrow.png' className='arrow-icon' alt='arrow icon' style={{transform: 'rotate(180deg)'}}/></a>
+                    </div>
+                    <InputSection title={props.title} handleSelectedItem={handleSelectedItem} isOpen={isOpen}/>
+                </div>
+            </div>
+        );
+    }
+    
 }
 
-const StyledTable = styled.table `
-    
-`;
 export default InputBox
