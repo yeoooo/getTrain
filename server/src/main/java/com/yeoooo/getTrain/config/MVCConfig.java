0364185ca77149f00128939c74e37effb1d6dd12
yeoooo <ruisya330@gmail.com>
2023-10-10 22:54:32 +0900

    Fix : CORS 방지 코드 추가
    allowedOrigins를 클라이언트의 localhost:port 값으로 설정 - 차 후 AWS의 주소값으로 변경되어야 함.

--- server/src/main/java/com/yeoooo/getTrain/config/MVCConfig.java
+++ server/src/main/java/com/yeoooo/getTrain/config/MVCConfig.java
@@ -4,12 +4,22 @@ import com.yeoooo.getTrain.util.MailUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableScheduling
-public class MVCConfig {
+public class MVCConfig implements WebMvcConfigurer {
     @Bean
     public MailUtil mailUtil() {
         return new MailUtil();
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .maxAge(3600);
     }
 }
