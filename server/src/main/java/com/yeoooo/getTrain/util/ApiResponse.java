a078f6eca7922918a30a486bed946e31f1821a44
yeoooo <ruisya330@gmail.com>
2023-10-28 00:04:50 +0900

    mod : 변수명 변경
    더욱 적절한 변수명으로 변경
    상태코드 변수 code -> status 로 변경

--- server/src/main/java/com/yeoooo/getTrain/util/ApiResponse.java
+++ server/src/main/java/com/yeoooo/getTrain/util/ApiResponse.java
@@ -1,25 +1,26 @@
 package com.yeoooo.getTrain.util;
 
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
+@ToString
 public class ApiResponse<T> {
 
     private T data ;
-    private HttpStatus code;
+    private HttpStatus status;
     private LocalDateTime timeStamp;
-    public ApiResponse(HttpStatus code, T data) {
-        this.code = code;
+    public ApiResponse(HttpStatus status, T data) {
+        this.status = status;
         this.data = data;
         this.timeStamp = LocalDateTime.now();
     }
 
-    public ApiResponse(HttpStatus code) {
-        this.code = code;
+    public ApiResponse(HttpStatus status) {
+        this.status = status;
         this.timeStamp = LocalDateTime.now();
     }
 
@@ -30,5 +31,6 @@ public class ApiResponse<T> {
     public static <T> ApiResponse fail(HttpStatus code, T errData) {
         return new ApiResponse<>(code, errData);
     }
+
 
 }
