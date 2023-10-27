0ac3387048ca3982e4592edc1dff13b8a74c930c
yeoooo <ruisya330@gmail.com>
2023-10-28 00:09:31 +0900

    mod : 상태 코드 변경
    상태 코드를 가독성이 좋은 알파벳으로 변경

--- client/src/@components/login/loginCard.jsx
+++ client/src/@components/login/loginCard.jsx
@@ -63,13 +63,13 @@ function LoginCard(){
                     }
                 }),
             }).then((res) => res.json());
-
-            if (response.status === 200) {
+            console.log(response)
+            if (response.status === 'OK') {
                 sessionStorage.setItem("email", email);
                 window.alert("로그인이 되었습니다!");
                 navigate('/search');
 
-            } else if (response.code === 401) {
+            } else if (response.status === 'UNAUTHORIZED') {
                 window.alert("등록되어 있지 않은 아이디 및 비밀번호입니다 :(");
                 window.alert(response.data);
             } else {
